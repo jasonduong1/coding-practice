fd09bda5bea955e516f2d6e42b424f94cd9dc4a5
jason <jduong89@gmail.com>
2022-06-02 14:18:17 -0700

    adds solution

--- merge_two_list.rb
+++ merge_two_list.rb
@@ -11,3 +11,14 @@
 # Example 3:
 # Input: list1 = [], list2 = [0]
 # Output: [0]
+
+def merge_two_lists(list1, list2)
+  merge_list = list1 + list2
+  return merge_list.sort
+end
+
+# p [1, 2, 3].push(*[2, 3, 4]).sort
+# p [1, 2, 3] + [2, 3, 4]
+p merge_two_lists([1, 2, 4], [1, 3, 4])
+p merge_two_lists([], [])
+p merge_two_lists([], [0])
