48cbf32d4544b8cfa88ecca6d8fba830b780e7b2
jason <jduong89@gmail.com>
2022-06-03 14:08:07 -0700

    adds solution

--- implement_str_str.rb
+++ implement_str_str.rb
@@ -1,15 +1,23 @@
-28.Implement strStr()
+# 28.Implement strStr()
 
-Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-Example 1:
-Input: haystack = "hello", needle = "ll"
-Output: 2
+# Example 1:
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
 
-Example 2:
-Input: haystack = "aaaaa", needle = "bba"
-Output: -1
+# Example 2:
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
 
 def str_str(haystack, needle)
-    
+  contain = haystack.include?(needle)
+  if contain
+    return haystack.index(needle)
+  else
+    return -1
+  end
 end
+
+p str_str("hello", "ll")
+p str_str("aaaaa", "bba")
