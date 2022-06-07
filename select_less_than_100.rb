df8e5490a3ad421bb208aba9f38a568f611f44b5
jason <jduong89@gmail.com>
2022-06-07 15:10:39 -0700

    adds solution

--- select_less_than_100.rb
+++ select_less_than_100.rb
@@ -2,3 +2,15 @@
 
 # Input: [99, 101, 88, 4, 2000, 50]
 # Output: [99, 88, 4, 50]
+
+def lessthan100(input)
+  output = []
+  input.each do |num|
+    if num < 100
+      output << num
+    end
+  end
+  return output
+end
+
+p lessthan100([99, 101, 88, 4, 2000, 50])
