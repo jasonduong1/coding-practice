70a2981d0747e603d9d66850d4b42814fc82fa39
jason <jduong89@gmail.com>
2022-06-07 15:32:18 -0700

    adds solution

--- reduce_product.rb
+++ reduce_product.rb
@@ -4,3 +4,14 @@
 # Output: 24
 
 # Explanation: (1 x 2 x 3 x 4) = 24
+
+# p [1, 2, 3, 4].reduce(1) { |product, num| product * num }
+# p [1, 2, 3, 4].reduce(:*)
+
+def product(input)
+  product = 1
+  input.each { |num| product *= num }
+  product
+end
+
+p product([1, 2, 3, 4])
