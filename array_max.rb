9738c12c6b4e37295b3fc89ac453d8600b68f8c4
jason <jduong89@gmail.com>
2022-06-07 15:22:39 -0700

    adds solution

--- array_max.rb
+++ array_max.rb
@@ -2,3 +2,17 @@
 
 # Input: [5, 17, -4, 20, 12]
 # Output: 20
+
+def max(input)
+  current_max = input[0]
+  index = 1
+  while index < input.length
+    if current_max < input[index]
+      current_max = input[index]
+    end
+    index += 1
+  end
+  current_max
+end
+
+p max([5, 17, -4, 20, 12])
