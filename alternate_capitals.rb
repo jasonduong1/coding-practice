be09b4fd65fcbeb4595f1521c7d5408d9741539f
jason <jduong89@gmail.com>
2022-06-08 14:46:49 -0700

    adds solutions with amazing each loop with index

--- alternate_capitals.rb
+++ alternate_capitals.rb
@@ -2,3 +2,28 @@
 
 #   Input: “hello, how are your porcupines today?”
 #   Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+
+# def alternate(input)
+#   input_array = input.split("")
+#   output_array = []
+#   index = 0
+#   while index < input_array.length
+#     if index.even?
+#       output_array << input_array[index].downcase
+#     else
+#       output_array << input_array[index].upcase
+#     end
+#     index += 1
+#   end
+#   output_array.join
+# end
+
+def alternate(input)
+  (0...input.size).each do |index|
+    input[index] = index.even? ? input[index].downcase : input[index].upcase
+  end
+  input
+end
+
+p alternate("hello, how are your porcupines today?")
+p alternate("hello")
