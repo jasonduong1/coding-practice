c52c80fdab40bb891f4c11232d69ad4877f58a4d
jason <jduong89@gmail.com>
2022-06-08 13:56:26 -0700

    adds solution

--- reverse_array.rb
+++ reverse_array.rb
@@ -2,3 +2,16 @@
 
 # Input: [1, 2, 3, 4, 5]
 # Output: [5, 4, 3, 2, 1]
+
+def reverse(input)
+  # output = []
+  # index = input.length - 1
+  # while index > -1
+  #   output << input[index]
+  #   index -= 1
+  # end
+  # output
+  input.reverse!
+end
+
+p reverse([1, 2, 3, 4, 5])
