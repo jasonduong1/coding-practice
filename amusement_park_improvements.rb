9530c4afb4c11918749a1690de4a625e23f748b6
jason <jduong89@gmail.com>
2022-06-09 16:49:07 -0700

    adds problem

---
+++ amusement_park_improvements.rb
@@ -0,0 +1,39 @@
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  # Do not edit above methods, add your own methods below.
+
+  # rubocop:disable Naming/PredicateName
+  def has_pass?
+    raise "Please implement the Attendee#haspass? method"
+  end
+
+  # rubocop:enable Naming/PredicateName
+
+  def fits_ride?(ride_minimum_height)
+    raise "Please implement the Attendee#fits_ride? method"
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    raise "Please implement the Attendee#allowed_to_ride? method"
+  end
+end
+
+p Attendee.new(100).has_pass?
+# => false
+p Attendee.new(140).fits_ride?(100)
+# => true
+attendee = Attendee.new(100)
+attendee.issue_pass!(42)
+p attendee.allowed_to_ride?(120)
+# => false
