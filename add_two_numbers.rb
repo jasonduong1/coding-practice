b0f8dc18d7fcd8da87ce06c41fedcd36246fdfa5
jason <jduong89@gmail.com>
2022-06-10 15:56:33 -0700

    adds class for testing

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -11,4 +11,14 @@ def add_two_numbers(l1, l2)
   return arr_sum
 end
 
-p add_two_numbers([2, 4, 3], [5, 6, 4])
+# p add_two_numbers([2, 4, 3], [5, 6, 4])
+
+class AddTwoNumbers
+  def add!(l1, l2)
+    reverse_int_l1 = l1.reverse.join.to_i
+    reverse_int_l2 = l2.reverse.join.to_i
+    reverse_int_sum = reverse_int_l1 + reverse_int_l2
+    reverse_arr_sum = reverse_int_sum.to_s.each_char.map(&:to_i)
+    arr_sum = reverse_arr_sum.reverse
+  end
+end
