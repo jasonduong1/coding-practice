9233c67f278f822dd03f7adb071b4af728816634
jason <jduong89@gmail.com>
2022-06-14 21:04:56 -0700

    adds solution and test for first two task

--- savings-account/savings_account.rb
+++ savings-account/savings_account.rb
@@ -1,10 +1,19 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    balance = 0.5
+    if balance < 0
+      interest_rate = -3.213
+    elsif balance < 1000
+      interest_rate = 0.5
+    elsif balance < 5000
+      interest_rate = 1.621
+    else
+      interest_rate = 2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise "Please implement the SavingsAccount.annual_balance_update method"
+    interest_rate = self.interest_rate(balance)
+    annual_balance = (interest_rate * 0.01 + 1) * balance
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
--- savings-account/savings_account_spec.rb
+++ savings-account/savings_account_spec.rb
@@ -4,7 +4,29 @@ RSpec.describe SavingsAccount do
   describe "self.interest_rate" do
     context 200.75 do
       it "returns 0.5" do
-        expect(SavingsAccount.interest_rate(200.75).to eq(0.5))
+        expect(SavingsAccount.interest_rate(200.75).to_s).to eq "0.5"
+      end
+    end
+    context -1 do
+      it "returns -3.213" do
+        expect(SavingsAccount.interest_rate(-1).to_s).to eq "-3.213"
+      end
+    end
+    context 4000 do
+      it "returns 1.621" do
+        expect(SavingsAccount.interest_rate(4000).to_s).to eq "1.621"
+      end
+    end
+    context 10000 do
+      it "returns -3.213" do
+        expect(SavingsAccount.interest_rate(10000).to_s).to eq "2.475"
+      end
+    end
+  end
+  describe "self.annual_balance_update" do
+    context 200.75 do
+      it "returns 201.75375" do
+        expect(SavingsAccount.annual_balance_update(200.75)).to be_within(0.00001).of(201.75375)
       end
     end
   end
