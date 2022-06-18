e860a894edb490e157991cc49b6410be74de5b94
jason <jduong89@gmail.com>
2022-06-17 17:35:38 -0700

    adds solution

--- boutique-inventory/boutique_inventory.rb
+++ boutique-inventory/boutique_inventory.rb
@@ -4,19 +4,36 @@ class BoutiqueInventory
   end
 
   def item_names
+    if @items == []
+      []
+    else
+      @items.map { |item| item[:name] }.sort
+    end
   end
 
   def cheap
+    cheap = []
+    @items.each { |item| cheap << item if item[:price] < 30 }
+    cheap
   end
 
   def out_of_stock
+    out_of_stock = []
+    @items.each { |item| out_of_stock << item if item[:quantity_by_size] == {} }
+    out_of_stock
   end
 
   def stock_for_item(name)
+    item = @items.find { |item| item[:name] == name }
+    item[:quantity_by_size]
   end
 
   def total_stock
-    raise "Implement the BoutiqueInventory#total_stock method"
+    total_stock = 0
+    @items.each do |item|
+      item[:quantity_by_size].each_value { |count| total_stock += count }
+    end
+    total_stock
   end
 
   private
