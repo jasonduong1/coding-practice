f3b7de314585ec39a6ce7cbe9fe0bd48c1c5155f
jason <jduong89@gmail.com>
2022-06-18 17:44:53 -0700

    adds first iteration of simple calculator

--- simple-calculator/simple_calculator.rb
+++ simple-calculator/simple_calculator.rb
@@ -1,7 +1,27 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ["+", "/", "*"].freeze
 
+  class UnsupportedOperation < StandardError
+  end
+
   def self.calculate(first_operand, second_operand, operation)
-    raise "Please implement the SimpleCalculator.calculate method"
+    if first_operand.is_a? String or second_operand.is_a? String
+      raise ArgumentError.new("Invalid argument!")
+    elsif operation == "+"
+      total = first_operand + second_operand
+      return "#{first_operand} + #{second_operand} = #{total}"
+    elsif operation == "/"
+      if second_operand == 0
+        return "Division by zero is not allowed."
+      else
+        total = first_operand / second_operand
+        return "#{first_operand} / #{second_operand} = #{total}"
+      end
+    elsif operation == "*"
+      total = first_operand * second_operand
+      return "#{first_operand} * #{second_operand} = #{total}"
+    else
+      raise UnsupportedOperation.new("Unsupported operation!")
+    end
   end
 end
