28393d63a2cb29fa22a24aecc282501678985930
jason <jduong89@gmail.com>
2022-06-18 17:43:21 -0700

    attempts the improvement

--- boutique-inventory/boutique_inventory_improvement.rb
+++ boutique-inventory/boutique_inventory_improvement.rb
@@ -1,10 +1,10 @@
+require "ostruct"
+
 class BoutiqueInventory
   attr_reader :items
 
   def initialize(items)
-    @items = items
-
-    raise "Refactor this code so that items is an array of openstructs"
+    @items = items.map { |item| OpenStruct.new(item) }
   end
 
   def item_names
@@ -21,3 +21,12 @@ class BoutiqueInventory
     raise "Refactor the code in total_stock"
   end
 end
+
+inventory = BoutiqueInventory.new([
+  { price: 65.00, name: "Maxi Brown Dress", quantity_by_size: { s: 3, m: 7, l: 8, xl: 4 } },
+  { price: 50.00, name: "Red Short Skirt", quantity_by_size: {} },
+  { price: 29.99, name: "Black Short Skirt", quantity_by_size: { s: 1, xl: 4 } },
+  { price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: { s: 7, m: 2 } },
+])
+
+p inventory.items
