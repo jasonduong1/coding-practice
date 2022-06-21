f7c4e112d13eec47be22d4a7066f2e6214fad5da
jason <jduong89@gmail.com>
2022-06-21 15:50:31 -0700

    adds solution but not sure how it works

--- primes/primes.rb
+++ primes/primes.rb
@@ -1,1 +1,10 @@
 # Write a function that returns whether a given number is a prime number.
+
+class Primes
+  def prime?(n)
+    return false if n < 2
+    (2..n / 2).none? { |i| n % i == 0 } #not sure what this does
+  end
+end
+
+p Primes.new.prime?(2)
