d6e9577f9641257c3c5420c17f97b3e7f783c3c3
jason <jduong89@gmail.com>
2022-06-21 15:21:39 -0700

    adds solution

--- reverse-word/reverse_word.rb
+++ reverse-word/reverse_word.rb
@@ -2,3 +2,13 @@
 
 # Input: “popcorn is so cool isn’t it yeah i thought so”
 # Output: “so thought i yeah it isn’t cool so is popcorn”
+
+class ReverseWord
+  def reverse(str)
+    arr = str.split(" ")
+    arr.reverse.join(" ")
+  end
+end
+
+p ReverseWord.new.reverse("hello there world")
+p ReverseWord.new.reverse("popcorn is so cool isn’t it yeah i thought so")
