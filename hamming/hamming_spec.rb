49dd43d08231d703ef4691ef22ec7fda26e083dd
jason <jduong89@gmail.com>
2022-06-20 18:42:15 -0700

    adds testing for hamming

--- hamming/hamming_spec.rb
+++ hamming/hamming_spec.rb
@@ -1,1 +1,19 @@
 require_relative "hamming"
+
+RSpec.describe Hamming do
+  context "same strings" do
+    it "returns 0" do
+      expect(described_class.new.difference("ABCDEFG", "ABCDEFG")).to eq(0)
+    end
+  end
+  context "strings with two differences" do
+    it "returns 2" do
+      expect(described_class.new.difference("ABCXEOG", "ABCDEFG")).to eq(2)
+    end
+  end
+  context "two empy strings" do
+    it "returns 0" do
+      expect(described_class.new.difference("", "")).to eq(0)
+    end
+  end
+end
