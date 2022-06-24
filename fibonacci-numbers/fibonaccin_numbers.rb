8b4487fc2b46e9def74e4ccb7feda79e84ae2378
jason <jduong89@gmail.com>
2022-06-23 21:50:01 -0700

    adds solution

--- fibonacci-numbers/fibonaccin_numbers.rb
+++ fibonacci-numbers/fibonaccin_numbers.rb
@@ -4,3 +4,26 @@
 
 # Input: 9
 # Output: 21 (as this is the 9th number of the Fibonacci Sequence)
+
+class FibonacciNumbers
+  def place(n)
+    array = [0, 1]
+    return output = 0 if n == 1
+    point = n - 2
+    point.times do
+      number = array[-1] + array[-2]
+      array << number
+    end
+    output = array[-1]
+  end
+end
+
+# p FibonacciNumbers.new.place(1)
+# p FibonacciNumbers.new.place(2)
+# p FibonacciNumbers.new.place(3)
+# p FibonacciNumbers.new.place(4)
+# p FibonacciNumbers.new.place(5)
+# p FibonacciNumbers.new.place(6)
+# p FibonacciNumbers.new.place(7)
+# p FibonacciNumbers.new.place(8)
+# p FibonacciNumbers.new.place(9)
