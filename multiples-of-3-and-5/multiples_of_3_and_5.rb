015eee2bdd0ef54bc1f3caed0f5c49ed7c9c7155
jason <jduong89@gmail.com>
2022-07-06 16:51:05 -0700

    adds solution

--- multiples-of-3-and-5/multiples_of_3_and_5.rb
+++ multiples-of-3-and-5/multiples_of_3_and_5.rb
@@ -1,3 +1,16 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
+
+def sumofmultiplesof3and5(number)
+  sum = 0
+  (3..number).each do |x|
+    if x % 3 == 0 || x % 5 == 0
+      sum += x
+    end
+  end
+  sum
+end
+
+p sumofmultiplesof3and5(9)
+p sumofmultiplesof3and5(999)
