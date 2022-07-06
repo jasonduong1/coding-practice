b300c3e375da0d105a1a521795ed534fbeb61e65
jason <jduong89@gmail.com>
2022-07-06 16:32:11 -0700

    adds solution

--- leap-year/leap_year.rb
+++ leap-year/leap_year.rb
@@ -6,3 +6,24 @@
 # except every year that is evenly divisible by 100
 # unless the year is also evenly divisible by 400
 # For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+class Year
+  def leap?(year)
+    if year % 4 == 0
+      if year % 100 == 0
+        if year % 400 == 0
+          return true
+        end
+        return false
+      end
+      return true
+    else
+      return false
+    end
+  end
+end
+
+# p Year.new.leap?(2000)
+# p Year.new.leap?(1997)
+# p Year.new.leap?(1996)
+# p Year.new.leap?(1900)
