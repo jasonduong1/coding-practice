33c84b9ff0934149ea1520273209091084a44b4d
jason <jduong89@gmail.com>
2022-07-08 00:16:05 -0700

    adds solution

--- array-mesh/one.rb
+++ array-mesh/one.rb
@@ -1,4 +1,14 @@
-Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
+# Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
 
-Input: ["a", "b", "c"], ["d", "e", "f", "g"]
-Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+# Input: ["a", "b", "c"], ["d", "e", "f", "g"]
+# Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+def mesh(arr1, arr2)
+  new_arr = []
+  arr1.each do |x|
+    arr2.each { |y| new_arr << (x + y) }
+  end
+  new_arr
+end
+
+p mesh(["a", "b", "c"], ["d", "e", "f", "g"])
