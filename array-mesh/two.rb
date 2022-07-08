b2be4a0258712b72f0a5b7ea97098796314fe8b9
jason <jduong89@gmail.com>
2022-07-08 00:29:22 -0700

    adds solution

--- array-mesh/two.rb
+++ array-mesh/two.rb
@@ -1,4 +1,22 @@
-Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
 
-Input: ["a", "b", "c", "d"]
-Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+# Input: ["a", "b", "c", "d"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+# set up new array
+# each loop do
+#   remove current element from the loop
+#   creates array without current element
+#   loop thru that array and add to output
+# end
+
+def singlemesh(arr)
+  output = []
+  arr.each do |x|
+    new_arr = arr - [x]
+    new_arr.each { |y| output << (x + y) }
+  end
+  output
+end
+
+p singlemesh(["a", "b", "c", "d"])
