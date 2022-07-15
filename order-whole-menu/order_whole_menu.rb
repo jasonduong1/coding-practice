45920d1935db242feeb4357b98a3aca7445782a4
jason <jduong89@gmail.com>
2022-07-14 23:30:35 -0700

    adds solution

--- order-whole-menu/order_whole_menu.rb
+++ order-whole-menu/order_whole_menu.rb
@@ -1,7 +1,15 @@
-Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
 
-Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
 
-Output: 14
+# Output: 14
 
-Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+def menu_total(hash)
+  total = 0
+  hash.each_value { |value| total += value }
+  total
+end
+
+p menu_total({ "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 })
