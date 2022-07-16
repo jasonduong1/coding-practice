df4baa450abd65f91ec43a2a549b211cd9b73a8d
jason <jduong89@gmail.com>
2022-07-16 15:19:17 -0700

    adds solution

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,15 +1,24 @@
-Given a DNA strand, return its RNA complement (per RNA transcription).
+# Given a DNA strand, return its RNA complement (per RNA transcription).
 
-Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+# Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
 
-Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
 
-G becomes C
-C becomes G
-T becomes A
-A becomes U
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
 
-So based on all this, here's a sample input/output:
+# So based on all this, here's a sample input/output:
 
-Input: 'ACGTGGTCTTAA'
-Output: 'UGCACCAGAAUU'
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
+
+def dna_to_rna(string)
+  sequence_to_rna = { "G" => "C", "C" => "G", "T" => "A", "A" => "U" }
+  rna = ""
+  string.chars.each { |dna| rna += sequence_to_rna[dna] }
+  rna
+end
+
+p dna_to_rna("ACGTGGTCTTAA")
