d748970b3a4ebd70370df4160e6ebac456e41df9
jason <jduong89@gmail.com>
2022-07-18 18:49:18 -0700

    adds solution

--- etl/two.rb
+++ etl/two.rb
@@ -1,75 +1,106 @@
-You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
+# You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
 
-Input:
+# Input:
 
-{
-1 => ["A", "E", "I", "O", "U"]
-}
+# {
+# 1 => ["A", "E", "I", "O", "U"]
+# }
 
-Output:
+# Output:
 
-{
-'a' => 1,
-'e' => 1,
-'i' => 1,
-'o' => 1,
-'u' => 1
-}
-Input:
+# {
+# 'a' => 1,
+# 'e' => 1,
+# 'i' => 1,
+# 'o' => 1,
+# 'u' => 1
+# }
+# Input:
 
-{
-1 => ["A", "E"],
-2 => ["D", "G"]
-}
+# {
+# 1 => ["A", "E"],
+# 2 => ["D", "G"]
+# }
 
-Output:
+# Output:
 
-{
-'a' => 1,
-'d' => 2,
-'e' => 1,
-'g' => 2
-}
+# {
+# 'a' => 1,
+# 'd' => 2,
+# 'e' => 1,
+# 'g' => 2
+# }
 
-Input:
+# Input:
 
-{
-1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-2 => ["D", "G"],
-3 => ["B", "C", "M", "P"],
-4 => ["F", "H", "V", "W", "Y"],
-5 => ["K"],
-8 => ["J", "X"],
-10 => ["Q", "Z"]
-}
+# {
+# 1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+# 2 => ["D", "G"],
+# 3 => ["B", "C", "M", "P"],
+# 4 => ["F", "H", "V", "W", "Y"],
+# 5 => ["K"],
+# 8 => ["J", "X"],
+# 10 => ["Q", "Z"]
+# }
 
-Output:
+# Output:
 
-{
-'a' => 1,
-'b' => 3,
-'c' => 3,
-'d' => 2,
-'e' => 1,
-'f' => 4,
-'g' => 2,
-'h' => 4,
-'i' => 1,
-'j' => 8,
-'k' => 5,
-'l' => 1,
-'m' => 3,
-'n' => 1,
-'o' => 1,
-'p' => 3,
-'q' => 10,
-'r' => 1,
-'s' => 1,
-'t' => 1,
-'u' => 1,
-'v' => 4,
-'w' => 4,
-'x' => 8,
-'y' => 4,
-'z' => 10
-}
+# {
+# 'a' => 1,
+# 'b' => 3,
+# 'c' => 3,
+# 'd' => 2,
+# 'e' => 1,
+# 'f' => 4,
+# 'g' => 2,
+# 'h' => 4,
+# 'i' => 1,
+# 'j' => 8,
+# 'k' => 5,
+# 'l' => 1,
+# 'm' => 3,
+# 'n' => 1,
+# 'o' => 1,
+# 'p' => 3,
+# 'q' => 10,
+# 'r' => 1,
+# 's' => 1,
+# 't' => 1,
+# 'u' => 1,
+# 'v' => 4,
+# 'w' => 4,
+# 'x' => 8,
+# 'y' => 4,
+# 'z' => 10
+# }
+
+def transform(hash)
+  output = {}
+  hash.each { |var| var[1].each { |letter| output[letter] = var[0] } }
+  output
+end
+
+p transform(
+  {
+    1 => ["A", "E", "I", "O", "U"],
+  }
+)
+
+p transform(
+  {
+    1 => ["A", "E"],
+    2 => ["D", "G"],
+  }
+)
+
+p transform(
+  {
+    1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2 => ["D", "G"],
+    3 => ["B", "C", "M", "P"],
+    4 => ["F", "H", "V", "W", "Y"],
+    5 => ["K"],
+    8 => ["J", "X"],
+    10 => ["Q", "Z"],
+  }
+)
