3b773bce05448de2d172e4b0f09dfb4e5bf6ab96
jason <jduong89@gmail.com>
2022-07-20 21:38:25 -0700

    adds solution

--- book-organizer/book_organizer.rb
+++ book-organizer/book_organizer.rb
@@ -1,40 +1,53 @@
-Given a list of books provided in this format:
+# Given a list of books provided in this format:
 
-[
-{title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
-{title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-{title: "1984", author: "George Orwell", year: 1949 },
-{title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
-{title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
-{title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-{title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
-]
+# [
+# {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+# {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+# {title: "1984", author: "George Orwell", year: 1949 },
+# {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+# {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+# {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+# {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+# ]
 
-return the data in this new author-centric format:
+# return the data in this new author-centric format:
 
-{ "J. R. R. Tolkien" => [
-{title: "The Lord of the Rings", year: 1954 },
-{title: "The Hobbit", year: 1937 },
-{title: "The Two Towers", year: 1954 }
-],
-"Harper Lee" => [
-{title: "To Kill a Mockingbird", year: 1960 },
-{title: "Go Set a Watchman", year: 2015 }
-],
-"George Orwell" => [
-{title: "1984", year: 1949 }
-],
-"F. Scott Fitzgerald" => [
-{title: "The Great Gatsby", year: 1925 }
-]
-}
+# { "J. R. R. Tolkien" => [
+# {title: "The Lord of the Rings", year: 1954 },
+# {title: "The Hobbit", year: 1937 },
+# {title: "The Two Towers", year: 1954 }
+# ],
+# "Harper Lee" => [
+# {title: "To Kill a Mockingbird", year: 1960 },
+# {title: "Go Set a Watchman", year: 2015 }
+# ],
+# "George Orwell" => [
+# {title: "1984", year: 1949 }
+# ],
+# "F. Scott Fitzgerald" => [
+# {title: "The Great Gatsby", year: 1925 }
+# ]
+# }
 
-p format([
-  {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
-  {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  {title: "1984", author: "George Orwell", year: 1949 },
-  {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
-  {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
-  {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-  {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
-  ])
+def format(array)
+  hash = {}
+  array.each do |book|
+    x = book[:author]
+    hash[x] = [] if !hash[x]
+    current_hash = {}
+    current_hash[:title] = book[:title]
+    current_hash[:year] = book[:year]
+    hash[x] << current_hash
+  end
+  hash
+end
+
+pp format([
+     { title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+     { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+     { title: "1984", author: "George Orwell", year: 1949 },
+     { title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+     { title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+     { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+     { title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 },
+   ])
