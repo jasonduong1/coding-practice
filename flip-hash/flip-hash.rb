29cde564357489c824c41c96c14e3e3343646779
jason <jduong89@gmail.com>
2022-07-20 20:42:16 -0700

    adds solution

--- flip-hash/flip-hash.rb
+++ flip-hash/flip-hash.rb
@@ -1,4 +1,12 @@
-Given a hash, create a new hash that has the keys and values switched.
+# Given a hash, create a new hash that has the keys and values switched.
 
-Input: {"a" => 1, "b" => 2, "c" => 3}
-Output: {1 => "a", 2 => "b", 3 => "c"}
+# Input: {"a" => 1, "b" => 2, "c" => 3}
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+def flip(hash)
+  output = {}
+  hash.each { |key, value| output[value] = key }
+  output
+end
+
+p flip({ "a" => 1, "b" => 2, "c" => 3 })
