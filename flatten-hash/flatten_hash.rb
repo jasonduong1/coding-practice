171bece7c8cc0ae8434583d7aca2a93b85485144
jason <jduong89@gmail.com>
2022-07-20 20:36:39 -0700

    adds solution

--- flatten-hash/flatten_hash.rb
+++ flatten-hash/flatten_hash.rb
@@ -1,4 +1,15 @@
-Given a hash, return a flat array containing all the hash’s keys and values.
+# Given a hash, return a flat array containing all the hash’s keys and values.
 
-Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
-Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+# Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
+# Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+
+def flatten(hash)
+  output = []
+  hash.each do |key, value|
+    output << key
+    output << value
+  end
+  output
+end
+
+p flatten({ "a" => 1, "b" => 2, "c" => 3, "d" => 4 })
