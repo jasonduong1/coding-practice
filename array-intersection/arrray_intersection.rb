b7ca6697d90628b22b66507c6e4f1ed768f14d59
jason <jduong89@gmail.com>
2022-07-27 14:15:02 -0700

    adds O(n) solution"

--- array-intersection/arrray_intersection.rb
+++ array-intersection/arrray_intersection.rb
@@ -1,7 +1,16 @@
-Given two arrays, return a new array that contains the intersection of the two arrays. The intersection means the values that are contained in both of the arrays. Do not use the "&", or any built-in intersection methods.
+# Given two arrays, return a new array that contains the intersection of the two arrays. The intersection means the values that are contained in both of the arrays. Do not use the "&", or any built-in intersection methods.
 
-NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+# NOTE: You must accomplish this in O(n) time. This is also known as linear time.
 
-Input: [1, 2, 3, 4, 5], [1, 3, 5, 7, 9]
-Output: [1, 3, 5]
+# Input: [1, 2, 3, 4, 5], [1, 3, 5, 7, 9]
+# Output: [1, 3, 5]
 
+def intersect(array1, array2)
+  hash = {}
+  new_array = []
+  array1.each { |number| hash[number] = true }
+  array2.each { |number| new_array << number if hash[number] }
+  new_array
+end
+
+p intersect([1, 2, 3, 4, 5], [1, 3, 5, 7, 9])
