d8dcde20e992005b4c493af985d3029208929b23
jason <jduong89@gmail.com>
2022-07-27 14:52:35 -0700

    adds solution

--- array-duplicate/array_duplicate.rb
+++ array-duplicate/array_duplicate.rb
@@ -1,6 +1,20 @@
-A given array has one pair of duplicate values. Return the first duplicate value.
+# A given array has one pair of duplicate values. Return the first duplicate value.
 
-NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+# NOTE: You must accomplish this in O(n) time. This is also known as linear time.
 
-Input: [5, 2, 9, 7, 2, 6]
-Output: 2
+# Input: [5, 2, 9, 7, 2, 6]
+# Output: 2
+
+def duplicate?(array)
+  hash = {}
+  array.each do |number|
+    if hash[number]
+      return number
+    else
+      hash[number] = true
+    end
+  end
+end
+
+p duplicate?([5, 2, 9, 7, 2, 6])
+p duplicate?([5, 2, 9, 7, 3, 6, 1, 1])
