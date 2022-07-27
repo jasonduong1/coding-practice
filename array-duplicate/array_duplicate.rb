678e8b58d9f0c000b5754c6032e57af07e8af149
jason <jduong89@gmail.com>
2022-07-27 14:36:54 -0700

    adds new problem

---
+++ array-duplicate/array_duplicate.rb
@@ -0,0 +1,6 @@
+A given array has one pair of duplicate values. Return the first duplicate value.
+
+NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+
+Input: [5, 2, 9, 7, 2, 6]
+Output: 2
