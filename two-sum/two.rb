76f4d82e1f0d0b5b35eab74212f25d4405af98b9
jason <jduong89@gmail.com>
2022-07-27 17:31:03 -0700

    adds solution, i impressed myself with this wone

--- two-sum/two.rb
+++ two-sum/two.rb
@@ -1,9 +1,19 @@
-This is the same exercise as Two Sum I, but you must now solve it in linear time.
+# This is the same exercise as Two Sum I, but you must now solve it in linear time.
 
-Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+# Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 
-Input: [2, 5, 3, 1, 0, 7, 11]
-Output: [3, 7]
+# Input: [2, 5, 3, 1, 0, 7, 11]
+# Output: [3, 7]
 
-Input: [1, 2, 3, 4, 5]
-Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+# Input: [1, 2, 3, 4, 5]
+# Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+def ten_sum(array)
+  hash = {}
+  array.each { |number| hash.key(number) ? (return [hash.key(number), number]) : hash[number] = 10 - number }
+  false
+end
+
+p ten_sum([2, 5, 3, 1, 0, 7, 11])
+p ten_sum([1, 2, 3, 4, 5])
+p ten_sum([10, 1, 11, 2, -1])
