cdd6fbd1f75d6d9e67dfcca0cc91fff7330878fc
jason <jduong89@gmail.com>
2022-07-29 15:26:13 -0700

    adds solution for anagrams with hash

--- anagrams/anagrams.rb
+++ anagrams/anagrams.rb
@@ -1,9 +1,31 @@
-Given two strings, return true if they are anagrams of each other, and false if they are not. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+# Given two strings, return true if they are anagrams of each other, and false if they are not. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 
-Do not use any built-in sort methods.
+# Do not use any built-in sort methods.
 
-Input: “silent”, “listen”
-Output: true
+# Input: “silent”, “listen”
+# Output: true
 
-Input: “frog”, “bear”
-Output: false
+# Input: “frog”, “bear”
+# Output: false
+
+def anagram?(string1, string2)
+  hash = {}
+  array1 = string1.split("")
+  array2 = string2.split("")
+  array1.each { |letter| hash[letter] ? hash[letter] += 1 : hash[letter] = 1 }
+  array2.each do |letter|
+    if hash[letter]
+      hash[letter] -= 1
+      if hash[letter] == -1
+        return false
+      end
+    else
+      return false
+    end
+  end
+  return true
+end
+
+p anagram?("silent", "listen")
+p anagram?("frog", "bear")
+p anagram?("rar", "arr")
