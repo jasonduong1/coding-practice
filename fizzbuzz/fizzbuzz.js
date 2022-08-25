ef8ef0dc2ea85b4f68befc6252ebc6dc1cca17f3
jason <jduong89@gmail.com>
2022-08-24 23:13:10 -0700

    adds attempt one

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,5 +1,18 @@
 var output = [];
+var count = 1;
 
 function fizzBuzz() {
-  output.push(output.length + 1);
+  if (count % 15 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+  count++;
+  console.log(output);
 }
+
+fizzBuzz();
