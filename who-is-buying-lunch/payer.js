096411be47499f956c059551e035bfa6355932e4
jason <jduong89@gmail.com>
2022-08-24 23:14:42 -0700

    adds context

---
+++ who-is-buying-lunch/payer.js
@@ -0,0 +1,6 @@
+// Who's Buying Lunch? Code Challenge
+// You are going to write a function which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+// Example Input
+// ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+// Example Output
+// Michael is going to buy lunch today!
