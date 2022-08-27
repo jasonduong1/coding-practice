bfe6c0b982832bb4a8cc8f2e054205312cda6d41
jason <jduong89@gmail.com>
2022-08-26 19:01:06 -0700

    returns string in place of array

--- rotational-cipher/rotational_cipher.rb
+++ rotational-cipher/rotational_cipher.rb
@@ -1,4 +1,4 @@
-class RotationCipher
+class RotationalCipher
   @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   def self.rotate(string, number)
     return (string) if number == 26 || number == 0
@@ -16,7 +16,7 @@ class RotationCipher
     string_array.each do |x|
       cipher_string_array << cipher_hash[x]
     end
-    return cipher_string_array
+    return cipher_string_array.join("")
   end
 end
 
@@ -24,6 +24,5 @@ p RotationCipher.rotate("the", 13)
 p RotationCipher.rotate("Omg", 5)
 p RotationCipher.rotate("hello there", 5)
 
-# need to convert array to string
 # does not currently account for capitalized chars
 # does not account for special characters
