4987f33b03fe6b354e5b6f983f5f728ed0a8a26d
jason <jduong89@gmail.com>
2022-08-26 22:40:22 -0700

    solves bottles of beers with while loop and if statement

--- bottles-of-beer/bottles-of-beer.js
+++ bottles-of-beer/bottles-of-beer.js
@@ -1,1 +1,24 @@
-function bottlesOfBeer() {}
+function bottlesOfBeer() {
+  var numberOfBottles = 99;
+  var bottleWord = "bottles";
+  while (numberOfBottles > 0) {
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer");
+    console.log("Take one down, pass it around,");
+    numberOfBottles--;
+    if (numberOfBottles === 1) {
+      bottleWord = "bottle";
+    }
+    if (numberOfBottles === 0) {
+      break;
+    }
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+  }
+  console.log("No more bottles of beer on the wall");
+  console.log("No more bottles of beer on the wall");
+  console.log("No more bottles of beer");
+  console.log("Go to the store and buy some more,");
+  console.log("99 bottles of beer on the wall");
+}
+
+bottlesOfBeer();
