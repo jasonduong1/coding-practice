da0d5830846a18ab9c8c2ba2cf68eea532861556
jason <jduong89@gmail.com>
2022-09-08 14:52:57 -0700

    adds solution to digit anagram

--- codesignal/digit-anagram.rb
+++ codesignal/digit-anagram.rb
@@ -32,3 +32,27 @@
 # a: [0, 5, 50, 7, 0, 550, 500, 55, 11]
 # Expected Output:
 # 1
+
+# compare first element sorted with remaining elements sorted
+# drop first element when compared
+# compare till array length is = 2
+# use summation (n*n +n)/2
+
+def anagram(array)
+  hash = {}
+  short_array = []
+  output = 0
+  array.each do |x|
+    sorted_x = x.to_s.split("").sort.join()
+    hash[sorted_x] ? hash[sorted_x] += 1 : hash[sorted_x] = 0
+  end
+  hash.delete_if { |_, v| v == 0 }
+  hash.map { |_, v| short_array << v }
+  short_array.each { |x| output += ((x * x + x) / 2) }
+  output
+end
+
+p anagram([25, 35, 872, 228, 53, 278, 872])
+p anagram([30, 72, 3, 227])
+p anagram([1, 1, 1, 1, 1])
+p anagram([0, 5, 50, 7, 0, 550, 500, 55, 11])
