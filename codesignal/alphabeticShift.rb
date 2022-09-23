f5defe375857b0513fda3131a6a2084e25148dab
jason <jduong89@gmail.com>
2022-09-22 20:34:12 -0700

    refactors alphabet array

--- codesignal/alphabeticShift.rb
+++ codesignal/alphabeticShift.rb
@@ -1,5 +1,5 @@
 def solution(inputString)
-  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a"]
+  alphabet = ("a".."z").to_a << "a"
   new_str = ""
   array = inputString.split("")
   for letter in array
