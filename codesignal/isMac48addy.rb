6b53c144d02061c1440f604e9d24fd495314315d
jason <jduong89@gmail.com>
2022-09-30 15:17:56 -0700

    refactors using only regex match

--- codesignal/isMac48addy.rb
+++ codesignal/isMac48addy.rb
@@ -1,4 +1,3 @@
 def solution(inputString)
-  arr = inputString.split("-")
-  inputString.count("-") != 5 ? false : arr.count != 6 ? false : arr.all? { |x| x.match(/^[A-F0-9]{2}$/) } ? true : false
+  inputString.match?(/^(\h{2}-){5}\h{2}$/)
 end
