c136789632d68193a1c47be7b6509abd8d0b1a7f
jason <jduong89@gmail.com>
2022-10-05 20:29:30 -0700

    refactors to one line using max, min

--- codesignal/more-practice/consecutiveArray.rb
+++ codesignal/more-practice/consecutiveArray.rb
@@ -1,5 +1,3 @@
 def solution(statues)
-  sorted_statues = statues.sort
-  amount_of_statues = sorted_statues[-1] - sorted_statues[0] + 1
-  amount_of_statues - statues.length
+  statues.max - statues.min - statues.length + 1
 end
