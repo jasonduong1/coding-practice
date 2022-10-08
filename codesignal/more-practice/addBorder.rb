849e5fce552277e066b36b84143ba15dd94f7b4d
jason <jduong89@gmail.com>
2022-10-07 20:39:07 -0700

    refactors by adding arrays together

--- codesignal/more-practice/addBorder.rb
+++ codesignal/more-practice/addBorder.rb
@@ -1,7 +1,5 @@
 def solution(picture)
-  framed_picture = []
   horizontal = "*" * (picture[0].size + 2)
   framed = picture.map { |pic| "*#{pic}*" }
-  framed_picture.push(horizontal, framed, horizontal)
-  framed_picture.flatten
+  [horizontal] + framed + [horizontal]
 end
