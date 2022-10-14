8aebfe9ff8f52b4fff6068cfdeaa91b9516d3f34
jason <jduong89@gmail.com>
2022-10-13 21:00:58 -0700

    refactors using to_a, map, to_s(2), count, and sum

--- codesignal/core/range-bit-count.rb
+++ codesignal/core/range-bit-count.rb
@@ -1,9 +1,3 @@
 def solution(a, b)
-  count = 0
-  arr = (a..b).map { |x| x.to_s(2) }
-  arr.each do |bin|
-    x = bin.split("")
-    x.each { |int| count += 1 if int == "1" }
-  end
-  count
+  (a..b).to_a.map { |x| x.to_s(2).count("1") }.sum
 end
