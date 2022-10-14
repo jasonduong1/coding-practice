b15b16b952d304a1ed3507f97508ed4a08dadb41
jason <jduong89@gmail.com>
2022-10-14 16:54:33 -0700

    refactors using map, ternary, and reduce

--- codesignal/core/apple-boxes.rb
+++ codesignal/core/apple-boxes.rb
@@ -1,7 +1,3 @@
 def solution(k)
-  difference = 0
-  for i in 1..k
-    i.odd? ? difference -= i ** 2 : difference += i ** 2
-  end
-  difference
+  (1..k).map { |x| x.odd? ? -1 * x * x : x * x }.reduce(:+)
 end
